7dc171113decfc5feaaa55039cc5d950e114a019
claireyywang <clairewang@openrobotics.org>
2019-08-22 16:25:47 -0700

    initial commit
    Signed-off-by: claireyywang <clairewang@openrobotics.org>

---
+++ set_env.py
@@ -0,0 +1,11 @@
+from distutils.sysconfig import get_python_lib
+import os
+import subprocess
+import sys
+
+
+pre = sys.argv[1]
+os.makedirs(get_python_lib(prefix=pre))
+os.putenv('PYTHONPATH',get_python_lib(prefix=pre))
+process = subprocess.call([sys.executable, 'setup.py', 'install', '--prefix='+pre])
+sys.stderr.write(repr(pre))
