7127b61a925a44be2aea8a73750497048f60be7f
claireyywang <clairewang@openrobotics.org>
2019-08-26 11:08:11 -0700

    update pythonpath for windows filepath format

--- set_env.py
+++ set_env.py
@@ -6,6 +6,6 @@ import sys
 
 pre = sys.argv[1]
 os.makedirs(get_python_lib(prefix=pre))
-os.putenv('PYTHONPATH',get_python_lib(prefix=pre))
+os.environ['PYTHONPATH'] = get_python_lib(prefix=pre)
 process = subprocess.call([sys.executable, 'setup.py', 'install', '--prefix='+pre])
 sys.stderr.write(repr(pre))
