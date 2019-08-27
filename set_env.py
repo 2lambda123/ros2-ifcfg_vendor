77e1c9d74c893a5de2fcd51f4593dd7cfd2f525d
claireyywang <clairewang@openrobotics.org>
2019-08-27 10:38:38 -0700

    modify windows path format
    Signed-off-by: claireyywang <clairewang@openrobotics.org>

--- set_env.py
+++ set_env.py
@@ -1,11 +1,12 @@
 from distutils.sysconfig import get_python_lib
 import os
+from pathlib import Path
 import subprocess
 import sys
 
 
 pre = sys.argv[1]
-os.makedirs(get_python_lib(prefix=pre))
-os.environ['PYTHONPATH'] = get_python_lib(prefix=pre)
+os.makedirs(Path(get_python_lib(prefix=pre)))
+os.environ['PYTHONPATH'] = Path(get_python_lib(prefix=pre))
 process = subprocess.call([sys.executable, 'setup.py', 'install', '--prefix='+pre])
 sys.stderr.write(repr(pre))
