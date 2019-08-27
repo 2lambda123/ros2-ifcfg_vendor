7296a2863d368bb0b3045c8b4a3b52a730e7baf8
claireyywang <clairewang@openrobotics.org>
2019-08-27 10:54:41 -0700

    change from winpath to str
    Signed-off-by: claireyywang <clairewang@openrobotics.org>

--- set_env.py
+++ set_env.py
@@ -7,6 +7,6 @@ import sys
 
 pre = sys.argv[1]
 os.makedirs(Path(get_python_lib(prefix=pre)))
-os.environ['PYTHONPATH'] = Path(get_python_lib(prefix=pre))
+os.environ['PYTHONPATH'] = get_python_lib(prefix=pre)
 process = subprocess.call([sys.executable, 'setup.py', 'install', '--prefix='+pre])
 sys.stderr.write(repr(pre))
